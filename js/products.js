8b4a1a3b8babd070a4bbe8c57538d5fb3dd7ec6a
axelalonso203 <agap.203@gmail.com>
2023-08-14 20:51:13 -0300

    mejorando el listado

--- js/products.js
+++ js/products.js
@@ -1,40 +1,51 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const url = "https://japceibal.github.io/emercado-api/cats_products/101.json";
-  
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const productosDiv = document.getElementById('productos');
+ddocument.addEventListener('DOMContentLoaded', function() {
+  const url = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const productosDiv = document.getElementById("productos")
+      
+      data.products.forEach(products => {
+        const productoDiv = document.createElement('div');
         
-        // Recorrer la colección de productos y mostrarlos en products.html
-        data.products.forEach(products => {
-          const productoDiv = document.createElement('div');
-          
-          const nombre = document.createElement('h2');
-          nombre.textContent = products.name;
-          
-          const precio = document.createElement('p');
-          precio.textContent = `Precio: $${products.cost}`;
-          
-          const descripcion = document.createElement('p');
-          descripcion.textContent = products.description;
-          
-          const cantidadVendidos = document.createElement('p');
-          cantidadVendidos.textContent = `Cantidad Vendida: ${products.soldCount}`;
-          
-          const imagen = document.createElement('img');
-          imagen.src = products.image;
-          
-          productoDiv.appendChild(nombre);
-          productoDiv.appendChild(precio);
-          productoDiv.appendChild(descripcion);
-          productoDiv.appendChild(cantidadVendidos);
-          productoDiv.appendChild(imagen);
-          
-          productosDiv.appendChild(productoDiv);
-        });
-      })
-      .catch(error => {
-        console.error('Error:', error);
+        const nombre = document.createElement('h2');
+        nombre.textContent = products.name;
+        
+        const precio = document.createElement('p');
+        precio.textContent = `Precio: $${products.cost}`;
+        
+        const descripcion = document.createElement('p');
+        descripcion.textContent = products.description;
+        
+        const cantidadVendidos = document.createElement('p');
+        cantidadVendidos.textContent = `${products.soldCount} vendidos`;
+        
+        const imagen = document.createElement('img');
+        imagen.src = products.image;
+
+        const contenido = `
+        <div class="row list-group-item d-flex justify-content-between">
+        <div class="col-3">
+          <img src="${imagen.src}" alt="${nombre.textContent}" class="img-thumbnail">
+        </div>
+        <div class="col-7">
+        <h3>${nombre.textContent}</h3>
+        <p>${descripcion.textContent}</p>
+        </div>
+        <div class="col-2">
+            <small>
+              ${cantidadVendidos.textContent}
+            </small>
+        </div>
+        </div>`
+        
+        productoDiv.innerHTML += contenido
+
+        productosDiv.appendChild(productoDiv)
       });
-  });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
