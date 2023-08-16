0ec8d8f6c7f8bbbf3845e203eaa3e96c03933c52
brunofleitas <brunofleitas2011@gmail.com>
2023-08-16 20:34:42 -0300

    index y login editados

--- js/index.js
+++ js/index.js
@@ -2,3 +2,18 @@ const session = localStorage.getItem("session");
 if (!session) {
     window.location.href = "login.html";
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("autos").addEventListener("click", function() {
+        localStorage.setItem("catID", 101);
+        window.location = "products.html"
+    });
+    document.getElementById("juguetes").addEventListener("click", function() {
+        localStorage.setItem("catID", 102);
+        window.location = "products.html"
+    });
+    document.getElementById("muebles").addEventListener("click", function() {
+        localStorage.setItem("catID", 103);
+        window.location = "products.html"
+    });
+});
--- js/login.js
+++ js/login.js
@@ -3,17 +3,3 @@ document.getElementById("button-login").onclick = function(){
     window.location.href = "index.html";
 };
 
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
-});
