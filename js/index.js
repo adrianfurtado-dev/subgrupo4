18a89648e1a74ce05c7aedbdab2f285d34aad3a5
brunofleitas <brunofleitas2011@gmail.com>
2023-08-16 20:19:50 -0300

    validacion funcionando

--- js/index.js
+++ js/index.js
@@ -1,14 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
-});
+const session = localStorage.getItem("session");
+if (!session) {
+    window.location.href = "login.html";
+}
