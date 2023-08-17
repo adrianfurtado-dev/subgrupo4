e7a89733e20dd582304fb6a44f6c58f09c54f39b
axelalonso203 <agap.203@gmail.com>
2023-08-17 18:48:34 -0300

    mejora al products.js

--- js/products.js
+++ js/products.js
@@ -7,42 +7,21 @@ document.addEventListener('DOMContentLoaded', function() {
       const productosDiv = document.getElementById("productos")
       
       data.products.forEach(products => {
-        const productoDiv = document.createElement('div');
-        
-        const nombre = document.createElement('h2');
-        nombre.textContent = products.name;
-        
-        const precio = document.createElement('p');
-        precio.textContent = `Precio: $${products.cost}`;
-        
-        const descripcion = document.createElement('p');
-        descripcion.textContent = products.description;
-        
-        const cantidadVendidos = document.createElement('p');
-        cantidadVendidos.textContent = `${products.soldCount} vendidos`;
-        
-        const imagen = document.createElement('img');
-        imagen.src = products.image;
-
-        const contenido = `
+        productosDiv.innerHTML += `
         <div class="row list-group-item d-flex justify-content-between">
         <div class="col-3">
-          <img src="${imagen.src}" alt="${nombre.textContent}" class="img-thumbnail">
+          <img src="${products.image}" alt="${products.name}" class="img-thumbnail">
         </div>
         <div class="col-7">
-        <h3>${nombre.textContent}</h3>
-        <p>${descripcion.textContent}</p>
+        <h3>${products.name}</h3>
+        <p>${products.description}</p>
         </div>
         <div class="col-2">
             <small>
-              ${cantidadVendidos.textContent}
+              ${products.soldCount}
             </small>
         </div>
         </div>`
-        
-        productoDiv.innerHTML += contenido
-
-        productosDiv.appendChild(productoDiv)
       });
     })
     .catch(error => {
