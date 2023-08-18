e82a6ae19ffd5cb02f09d1de6f38c4b2bcd1740f
Adrián Furtado <adrianfurtado.dev@gmail.com>
2023-08-18 09:53:09 -0300

    Cambios en el ojo en password

--- js/ux.js
+++ js/ux.js
@@ -3,10 +3,10 @@ const ojo = document.getElementById('ojo');
 
 function mostrar() { 
     if (contrasena.type == 'password') {
-        ojo.src = '../subgrupo4/img/ojo con barra.png';
+        ojo.src = '/subgrupo4/img/ojo con barra.png';
         contrasena.type = 'text';
     } else {
-        ojo.src = '../subgrupo4/img/ojo.png';
+        ojo.src = '/subgrupo4/img/ojo.png';
         contrasena.type = 'password';
     }
 }
