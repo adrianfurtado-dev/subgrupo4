df4b6df38bc477c8896974a9efb5be0b2f5226c7
brunofleitas <brunofleitas2011@gmail.com>
2023-08-31 18:30:52 -0300

    Busqueda vivo funcionando

--- js/products.js
+++ js/products.js
@@ -4,25 +4,44 @@ document.addEventListener('DOMContentLoaded', function() {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      const productosDiv = document.getElementById("productos")
-      
-      data.products.forEach(products => {
-        productosDiv.innerHTML += `
-        <div class="row list-group-item d-flex justify-content-between">
-        <div class="col-3">
-          <img src="${products.image}" alt="${products.name}" class="img-thumbnail">
-        </div>
-        <div class="col-7">
-        <h3>${products.name}</h3>
-        <p>${products.description}</p>
-        </div>
-        <div class="col-2">
-            <small>
-              ${products.soldCount}
-            </small>
-        </div>
-        </div>`
-      });
+      const productosDiv = document.getElementById("productos");
+      const searchInput = document.getElementById("searchInput");
+
+      // Función para filtrar los productos en tiempo real
+      function filterProducts() {
+        const searchTerm = searchInput.value.toLowerCase();
+        const filteredProducts = data.products.filter(product => {
+          const productName = product.name.toLowerCase();
+          const productDescription = product.description.toLowerCase();
+          return productName.includes(searchTerm) || productDescription.includes(searchTerm);
+        });
+
+        // Limpiar la lista de productos antes de mostrar los resultados filtrados
+        productosDiv.innerHTML = "";
+
+        // Mostrar los productos filtrados
+        filteredProducts.forEach(product => {
+          productosDiv.innerHTML += `
+            <div class="row list-group-item d-flex justify-content-between">
+              <div class="col-3">
+                <img src="${product.image}" alt="${product.name}" class="img-thumbnail">
+              </div>
+              <div class="col-7">
+                <h3>${product.name}</h3>
+                <p>${product.description}</p>
+              </div>
+              <div class="col-2">
+                <small>${product.soldCount}</small>
+              </div>
+            </div>`;
+        });
+      }
+
+      // Escuchar el evento input en el campo de búsqueda
+      searchInput.addEventListener('input', filterProducts);
+
+      // Mostrar todos los productos al cargar la página
+      filterProducts();
     })
     .catch(error => {
       console.error('Error:', error);
