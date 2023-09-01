4c5c5fbaf786333696726bd75164f822a7cf9fc1
Jorge Pirez <jorge.a.pirez@gmail.com>
2023-08-31 22:20:32 -0300

    login centrado y arreglos al signup

---
+++ js/signup.js
@@ -0,0 +1,134 @@
+const loginf = document.getElementById('divformlogin')
+const loginbtn = document.getElementById('loginbtn')
+const Msignup = document.getElementById('formsignup')
+const signupbtn = document.getElementById('registro')
+const btndarkmode = document.querySelector('#boton-darkmode')
+//Determina la fuerza de la contraseña según los criterios dentro de la función
+
+function fuerza(contraseña){
+    let i=0
+    if(contraseña.length > 6){
+        i++
+    }
+    if(contraseña.length >=10){
+        i++
+    }
+    if(/[A-Z]/.test(contraseña)){
+        i++
+    }
+    if(/[0-9]/.test(contraseña)){
+        i++;
+    }
+    if(/[A-Za-z0-8]/.test(contraseña)){
+        i++
+    }
+    return i;
+}
+let medidor = document.querySelector('.medidor')
+let barra = document.querySelector('.fuerza')
+var str = document.getElementById('ParrafoF')
+//Aplica las clases a la contraseña según se vaya actualizando
+
+document.addEventListener('keyup', function(e){
+    let contraseña = document.querySelector('#contraseñaregistro').value;
+    let barra = fuerza(contraseña)
+    if(barra ===0){
+        medidor.style.display = 'none'
+        
+    } else if(barra >0) {
+        medidor.style.display = 'block'
+        
+    }
+    if( barra <= 2){
+        medidor.classList.add('debil')
+        medidor.classList.remove('medio')
+        medidor.classList.remove('fuerte')
+        str.innerHTML = "La contraseña es débil"
+        
+    } else if( barra >= 2 && barra <=4){
+        medidor.classList.remove('debil')
+        medidor.classList.add('medio')
+        medidor.classList.remove('fuerte')
+        str.innerHTML = "La contraseña es moderada"
+        
+    } else{
+        medidor.classList.remove('debil')
+        medidor.classList.remove('medio')
+        medidor.classList.add('fuerte')
+        str.innerHTML = "La contraseña es fuerte"
+        
+
+        }
+    
+})
+const contraseñaregistro = document.querySelector('#contraseñaregistro')
+const requisitos = document.querySelector('.requisitos-contraseña')
+//Muestra una tabla con sugerencias para la contraseña
+contraseñaregistro.addEventListener('click', () =>{
+    requisitos.style.display = 'block'
+})
+//Si se clickea fuera del campo de texto, desaparece la tabla
+
+document.addEventListener('click', (event) =>{
+    if(event.target !== contraseñaregistro){
+        requisitos.style.display ='none'
+    }
+})
+const signup = document.getElementById('signupform');
+const mensajeError = document.getElementById('mensaje-error')
+//Validación de si los campos estan rellenados y el email cumple el formato + envío
+signup.addEventListener('submit', function(event){
+    event.preventDefault();
+    const usuario= document.getElementById('registrousuario').value;
+    const email = document.getElementById('emailregistro').value;
+    const password = document.getElementById('contraseñaregistro').value;
+    const fecha = document.getElementById('fecha').value;
+    if (usuario === '' || email === '' || password === '' || fecha === ''){
+        mensajeError.textContent = 'Se requiere llenar todos los campos'
+        return;
+    }
+    const formatoemail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!formatoemail.test(email)){
+        mensajeError.textContent = 'Ingrese una dirección email valida';
+        return;
+    }
+    mensajeError.textContent = ''
+    signup.submit()
+    
+    window.location.href = 'index.html'
+    Msignup.reset();
+});
+//Verifica si ambas contraseñas son iguales, en caso de serlo imprime un mensaje, en caso contrario no actua.
+const repitacontraseñainput = document.getElementById('repitacontraseña');
+repitacontraseñainput.addEventListener('input', () => {
+    const contraseña = document.getElementById('contraseñaregistro').value;
+    const repitacontraseña = repitacontraseñainput.value;
+    if (contraseña !== repitacontraseña) {
+        mensajeError.textContent = 'Las contraseñas no coinciden';
+        mensajeError.style.color = 'red';
+    } else {
+        mensajeError.textContent = '';
+    }
+    if ( contraseña && repitacontraseña === ''){
+        mensajeError.textContent = ''
+    }
+});
+//Se mostrara el formulario de registro cuando se presione el enunciado registrese aquí, y volvera al login si se presiona en Ingrese aquí.
+function mostrarregistro() {
+    loginf.style.display = 'none';
+    Msignup.style.display = 'block';
+    btndarkmode.style.bottom = '5px';
+}
+
+function mostrarlogin() {
+    Msignup.style.display = 'none';
+    loginf.style.display = 'block';
+    btndarkmode.style.bottom = '15rem';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    event.preventDefault();
+    signupbtn.addEventListener('click', mostrarregistro);
+    loginbtn.addEventListener('click', mostrarlogin);
+});
+  
--- js/ux.js
+++ js/ux.js
@@ -9,6 +9,14 @@ function mostrar() {
         ojo.src = '/img/ojo.png';
         contrasena.type = 'password';
     }
+    if (ojo.classList.contains("fa-eye")) {
+        ojo.classList.remove("fa-eye");
+        ojo.classList.add("fa-eye-slash");
+    } else {
+        ojo.classList.remove("fa-eye-slash");
+        ojo.classList.add("fa-eye");
+    }
+
 }
 
 ojo.addEventListener('click', mostrar);
@@ -18,6 +26,19 @@ const h1= document.querySelector("h1")
 const form = document.querySelector("form")
 const recordarbox = document.querySelector("#recordarcontraseñaosc")
 const pespecial = document.querySelector("#registradoosc")
+const registroform = document.querySelector("#signupform")
+const pregistrado = document.querySelector('#yaregistrado')
+const usuario = document.querySelector('#usuario')
+const contraseñap= document.querySelector('#contrasena')
+const probando = document.querySelector('#registrousuario')
+const fecha = document.querySelector('#fecha')
+const ingreseE = document.querySelector('#emailregistro')
+const registcontraseña = document.querySelector('#contraseñaregistro')
+const reppitcontraseña = document.querySelector('#repitacontraseña')
+const calendario = document.querySelector('input[type="date"]')
+const parrafoF = document.querySelector('#ParrafoF')
+
+
 
 botondarkmode.addEventListener("click", () => {
     body.classList.toggle("dark-mode");
@@ -25,16 +46,48 @@ botondarkmode.addEventListener("click", () => {
     form.classList.toggle("dark-mode")
     recordarbox.classList.toggle("luz")
     pespecial.classList.toggle("luz")
+    registroform.classList.toggle("dark-mode")
+    pregistrado.classList.toggle("luz")
+    usuario.classList.toggle("activep")
+    contraseñap.classList.toggle("activep")
+    title.classList.toggle("luz")
+    ojo.classList.toggle("activer")
+    probando.classList.toggle("pblanco")
+    fecha.classList.toggle("fechac")
+    ingreseE.classList.toggle("eblanco")
+    registcontraseña.classList.toggle("cblanco")
+    reppitcontraseña.classList.toggle("rblanco")
+    calendario.classList.toggle("activecal")
+    parrafoF.classList.toggle("luz")
+    requisitos.classList.toggle("luz")
     botondarkmode.classList.toggle("active")
-});
-botondarkmode.addEventListener('click', () =>{
     if (body.classList.contains("dark-mode")) {
         localStorage.setItem("darkMode", "on");
+        terminos.classList.toggle("luz")
     } else {
         localStorage.setItem("darkMode", "off");
     }
 });
+const botondarkmodeRegistro = document.querySelector("#boton-darkmode-registro");
+const formregistro = document.querySelector('#signupform')
+const terminos = document.getElementById('terminos')
+botondarkmodeRegistro.addEventListener("click", () => {
+    body.classList.toggle("dark-mode")
+    formregistro.classList.toggle("dark-mode")
+    pregistrado.classList.toggle("luz")
+    terminos.classList.toggle("luz")
+    probando.classList.toggle("pblanco")
+    fecha.classList.toggle("fechac")
+    ingreseE.classList.toggle("eblanco")
+    registcontraseña.classList.toggle("cblanco")
+    reppitcontraseña.classList.toggle("rblanco")
+    calendario.classList.toggle("activecal")
+    parrafoF.classList.toggle("luz")
+    requisitos.classList.toggle("luz")
+    botondarkmodeRegistro.classList.toggle("active")
+    
 
+});
 
 
 
