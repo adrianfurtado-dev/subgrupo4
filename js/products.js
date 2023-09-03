54df328c280dc2e03794a050b58cfc5458628128
Adrián Furtado <adrianfurtado.dev@gmail.com>
2023-09-03 16:42:24 -0300

    Error al cargar productoes en product.js arreglado

--- js/products.js
+++ js/products.js
@@ -29,7 +29,7 @@ function listTitle(catName) {
 
 function listProducts(products) {
     productosDiv.innerHTML = "";
-    let resultFound = false; 
+    let resultFound = false;
 
     products.forEach(product => {
         if (((minPrice == undefined) || (minPrice != undefined && parseInt(product.cost) >= minPrice)) &&
@@ -49,7 +49,7 @@ function listProducts(products) {
                         </small>
                     </div>
                 </div>`;
-            resultFound = true; 
+            resultFound = true;
         }
     });
 
@@ -94,7 +94,7 @@ function sorters() {
         if (isValidFilter(ORDER_BY_PROD_PRICE))
             sortAndShowProducts(ORDER_BY_PROD_PRICE);
         else
-            listProducts([]); 
+            listProducts([]);
     });
 
     document.getElementById("sortByCount").addEventListener("click", function () {
@@ -162,54 +162,16 @@ clearBtn.addEventListener('click', clearPriceInputs);
 
 document.addEventListener('DOMContentLoaded', documentLoaded);
 
-document.addEventListener('DOMContentLoaded', function() {
-    const catID = getCatID();
-    const url = APIUrl(catID);
-  
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const productosDiv = document.getElementById("productos");
-        const searchInput = document.getElementById("searchInput");
-  
-        // Función para filtrar los productos en tiempo real
-        function filterProducts() {
-          const searchTerm = searchInput.value.toLowerCase();
-          const filteredProducts = data.products.filter(product => {
-            const productName = product.name.toLowerCase();
-            const productDescription = product.description.toLowerCase();
-            return productName.includes(searchTerm) || productDescription.includes(searchTerm);
-          });
-  
-          // Limpiar la lista de productos antes de mostrar los resultados filtrados
-          productosDiv.innerHTML = "";
-  
-          // Mostrar los productos filtrados
-          filteredProducts.forEach(product => {
-            productosDiv.innerHTML += `
-              <div class="row list-group-item d-flex justify-content-between">
-                <div class="col-3">
-                  <img src="${product.image}" alt="${product.name}" class="img-thumbnail">
-                </div>
-                <div class="col-7">
-                  <h3>${product.name}</h3>
-                  <p>${product.description}</p>
-                </div>
-                <div class="col-2">
-                  <small>${product.soldCount}</small>
-                </div>
-              </div>`;
-          });
-        }
-  
-        // Escuchar el evento input en el campo de búsqueda
-        searchInput.addEventListener('input', filterProducts);
-  
-        // Mostrar todos los productos al cargar la página
-        filterProducts();
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  });
+// Función para filtrar los productos en tiempo real
+function filterProducts() {
+    const searchTerm = searchInput.value.toLowerCase();
+    const filteredProducts = currentProductsArray.filter(product => {
+        const productName = product.name.toLowerCase();
+        const productDescription = product.description.toLowerCase();
+        return productName.includes(searchTerm) || productDescription.includes(searchTerm);
+    });
+    listProducts(filteredProducts);
+}
+// Escuchar el evento input en el campo de búsqueda
+searchInput.addEventListener('input', filterProducts);
 
