5e42d10f3b05c529719cc0b66f218fceaa747ab2
Adrián Furtado <adrianfurtado.dev@gmail.com>
2023-09-17 17:42:30 -0300

    Problemas con la puntuación (nuevamente arreglados)

--- js/product-info.js
+++ js/product-info.js
@@ -167,10 +167,19 @@ if (formComen) {
 }
 
 newRating.addEventListener('input', () => {
-  newRating.value = newRating.value.replace(/[^0-9.]/g, '')
+  //newRating.value = newRating.value.replace(/[^0-9.]/g, '')
+  const value = newRating.value;
+  const isValid = /^-?\d*\.?\d*$/.test(value);
 
-  if (newRating.value > 5) {
-    let stringRating = newRating.value.toString();
+  if(!isValid) {
+    newRating.value = '';
+  }
+  if(value > 5) {
+    let stringRating = newRating.value.toString(); 
     newRating.value = stringRating.slice(-1) > 5 ? 5 : stringRating.slice(-1)
   }
+  /*if (parseFloat(newRating.value) > 5) {
+    let stringRating = newRating.value.toString(); 
+    newRating.value = stringRating.slice(-1) > 5 ? 5 : stringRating.slice(-1)
+  }*/
 });
