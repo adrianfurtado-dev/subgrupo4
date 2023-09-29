a54c0f7de236c6619449b50e1eaeae3c619aca50
brunofleitas <brunofleitas2011@gmail.com>
2023-09-30 01:00:04 +0200

    Se creo modo dia-noche en desplegable

--- js/product-info.js
+++ js/product-info.js
@@ -18,6 +18,7 @@ function stars(puntaje) {
   return starOuter;
 }
 
+/*
 const showProduct = (data) => {
   const imgs = data.images
     .map((element, index) => `
@@ -71,7 +72,52 @@ const requestToAPI = (URL) => {
     .then((response) => response.json())
     .then((data) => showProduct(data))
     .catch((error) => console.error('Error displaying product: ', error));
+}; 
+*/
+
+const showProduct = (data) => {
+  const imgs = data.images
+    .map((element) => `<img src="${element}" width="250">`)
+    .join('');
+
+  container. innerHTML = `
+    <h2 class="my-3">${data. name}</h2>
+    <hr/>
+    <div class="d-flex flex-column gap-3">
+      <div>
+        <strong>Precio</strong><br/>
+        <span>${data.currency} ${data.cost}</span>
+      </div>
+      <div>
+        <strong>Descripción</strong><br/>
+        <span>${data.description}</span>
+      </div>
+      <div>
+        <strong>Categoría</strong><br/>
+        <span>${data.category}</span>
+      </div>
+      <div>
+        <strong>Cantidad de vendidos</strong><br/>
+        <span>${data.soldCount}</span>
+      </div>
+      <div>
+        <strong>Imágenes ilustrativas</strong><br/>
+        <div class="d-flex gap-2">
+          ${imgs}
+        </div>
+      </div>
+    </div>
+  `;
+  loadComments(productID);
 };
+
+const requestToAPI = (URL) => {
+  fetch (URL)
+    .then((response) => response. json())
+    .then ((data) => showProduct(data))
+    .catch((error) => console.error('Error displaying product: ', error));
+};
+
 
 const loadComments = (productID) => {
   if (productID) {
