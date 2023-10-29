bb8f6c3017070b63be742666c2e767e31b042f60
Melanie Luzardo <mel.luzardo9@gmail.com>
2023-10-29 15:06:16 -0300

    Cartel de compra realizada

--- js/cart.js
+++ js/cart.js
@@ -404,7 +404,14 @@ function validateAccountNumber() {
   }
 }
 
+let popup = document.getElementById("popup");
 
+function openPopup(){
+  popup.classList.add("open-popup");
+}
+function closePopup(){
+  popup.classList.remove("open-popup");
+}
 
 
 
