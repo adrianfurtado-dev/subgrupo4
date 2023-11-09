cb0e324d34557f7792aacab19bbae64179018d83
axelalonso203 <agap.203@gmail.com>
2023-11-09 17:31:11 -0300

    Added email to the profile page

--- js/my-profile.js
+++ js/my-profile.js
@@ -4,3 +4,13 @@ let inputFile = document.getElementById("input-file");
 inputFile.onchange = function(){
     profilePic.src = URL.createObjectURL(inputFile.files[0]);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    let emailValue = sessionStorage.getItem("email");
+
+    // Asignar el valor al campo de texto 'emailperfil'
+    let emailProfile = document.getElementById('emailperfil');
+    if (emailValue) {
+        emailProfile.value = emailValue;
+    }
+})
