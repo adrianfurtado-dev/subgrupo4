33e26f7b751b38a4017ae4a8317e8708995765b9
brunofleitas <brunofleitas2011@gmail.com>
2023-11-11 05:53:33 +0100

    Carga de los datos de usuario
    Se completó la carga de datos dd usuario en el localstorage.

--- js/my-profile.js
+++ js/my-profile.js
@@ -12,7 +12,26 @@ document.addEventListener('DOMContentLoaded', () => {
     let emailProfile = document.getElementById('emailperfil');
     if (emailValue) {
         emailProfile.value = emailValue;
+        loadUserData();
     } else {
         window.location.href = '/login.html'
     }
 })
+
+// Función para cargar los datos del usuario en el formulario
+function loadUserData() {
+    // Obtener datos almacenados localmente
+    let storedData = JSON.parse(localStorage.getItem("userData"));
+    if (storedData) {
+        document.getElementById('firstname').value = storedData.firstName;
+        document.getElementById('secondname').value = storedData.secondName;
+        document.getElementById('lastname').value = storedData.lastName;
+        document.getElementById('secondlastname').value = storedData.secondLastName;
+        document.getElementById('phonenumber').value = storedData.phoneNumber;
+    }
+}
+
+
+
+
+
