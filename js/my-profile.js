963a71cdb13f7f245cf9536e12a23f0c080ac5b1
adrianfurtado-dev <adrianfurtado.dev@gmail.com>
2023-11-12 10:04:41 -0300

    Cambios de Noe en my-profile.js y my-profile.html

--- js/my-profile.js
+++ js/my-profile.js
@@ -12,26 +12,92 @@ document.addEventListener('DOMContentLoaded', () => {
     let emailProfile = document.getElementById('emailperfil');
     if (emailValue) {
         emailProfile.value = emailValue;
-        loadUserData();
+        saveData();
     } else {
         window.location.href = '/login.html'
     }
 })
 
-// Función para cargar los datos del usuario en el formulario
-function loadUserData() {
-    // Obtener datos almacenados localmente
-    let storedData = JSON.parse(localStorage.getItem("userData"));
-    if (storedData) {
-        document.getElementById('firstname').value = storedData.firstName;
-        document.getElementById('secondname').value = storedData.secondName;
-        document.getElementById('lastname').value = storedData.lastName;
-        document.getElementById('secondlastname').value = storedData.secondLastName;
-        document.getElementById('phonenumber').value = storedData.phoneNumber;
+const firstNameInput = document.getElementById("firstname");
+const secondNameInput = document.getElementById("secondname");
+const lastNameInput = document.getElementById("lastname");
+const secondLastNameInput = document.getElementById("secondlastname");
+const phoneInput = document.getElementById("phonenumber");
+
+firstNameInput.addEventListener("input", validateFirstName);
+lastNameInput.addEventListener("input", validateLastName);
+
+phoneInput.addEventListener("input", function () {
+    // Elimina cualquier carácter que no sea un número
+    this.value = this.value.replace(/\D/g, "");
+  });
+
+function onlyLetters(event) {
+    // Elimina cualquier carácter que no sea una letra o espacio
+    event.target.value = event.target.value.replace(/[^a-zA-Z\s]/g, "");
+}
+
+// Asignar la función a los eventos de entrada de cada input
+firstNameInput.addEventListener("input", onlyLetters);
+secondNameInput.addEventListener("input", onlyLetters);
+lastNameInput.addEventListener("input", onlyLetters);
+secondLastNameInput.addEventListener("input", onlyLetters);
+
+// Validar nombre y apellido
+function validateFirstName() {
+    if (firstNameInput.value.trim() === "") {
+      firstNameInput.classList.remove("is-valid");
+      firstNameInput.classList.add("is-invalid");
+    } else {
+      firstNameInput.classList.remove("is-invalid");
+      firstNameInput.classList.add("is-valid");
     }
 }
 
+function validateLastName() {
+    if (lastNameInput.value.trim() === "") {
+      lastNameInput.classList.remove("is-valid");
+      lastNameInput.classList.add("is-invalid");
+    } else {
+      lastNameInput.classList.remove("is-invalid");
+      lastNameInput.classList.add("is-valid");
+    }
+}
 
+// Validar campos requeridos para envio de formulario
+function validateRequiredFields() {
+    let requiredFields = document.querySelectorAll("input[required]");
+    let allValid = true;
 
+    requiredFields.forEach(field => {
+        if (field.value.trim() === "") {
+            field.classList.add("is-invalid");
+            allValid = false;
+        } else {
+            field.classList.remove("is-invalid");
+        }
+    });
 
+    return allValid;
+}
 
+// Almacenar en el localStorage
+function saveData() {
+    if (validateRequiredFields()) {
+        let userData = {
+            firstName: firstNameInput.value,
+            secondName: secondNameInput.value,
+            lastName: lastNameInput.value,
+            secondLastName: secondLastNameInput.value,
+            phoneNumber: phoneInput.value,
+        };
+
+        localStorage.setItem("userData", JSON.stringify(userData));
+        alert("Cambios guardados");
+    } else {
+        alert("Por favor, completa todos los campos obligatorios.");
+    }
+}
+
+// Asignar evento al botón de guardar
+document.querySelector('.btn-primary').addEventListener('click', saveData);
